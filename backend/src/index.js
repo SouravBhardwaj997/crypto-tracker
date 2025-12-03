c5987da36575d3b54d7aa073c699446617511d55
Sourav <souravbhardwaj997@gmail.com>
2025-12-03 18:40:30 +0530

    db connection made

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import "dotenv/config";
+import { connectDB } from "./utils/ConnectDb.js";
 const app = express();
 const PORT = process.env.PORT || 8001;
 app.listen(PORT, () => {
+  connectDB();
   console.log("Server is running at PORT:", PORT);
 });
